ce19d451c268ca87f732d5fed148cbfbcc9c6d08
Nick Scratch <drevilbones@gmail.com>
2025-05-05 20:56:33 -0400

    more tweaks

--- keyboards/sofle/keymaps/drevilbones/keymap.c
+++ keyboards/sofle/keymaps/drevilbones/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_GRV,   KC_A,   MT_S,    MT_D,    MT_F,    KC_G,                      KC_H,    MT_J,   MT_K,    MT_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  _______,   _______, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_LBRC, KC_RBRC, MO(3),   MO(2), KC_ENT,    MO(2),   KC_SPC,   MO(3),  KC_MINS, KC_EQL
+                    KC_LBRC, KC_RBRC, MO(3),   MO(2), KC_ENT,    MO(2),   KC_SPC,  MO(3),  KC_MINS, KC_EQL
 ),
 
 [GAME] = LAYOUT( //games (disabling the tap-hold keys and shifting wasd over to turn it into esdf)
@@ -67,21 +67,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_T,   KC_Q,    KC_W,    KC_E,    KC_R,                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LCTL,  KC_G,   KC_A,    KC_S,    KC_D,    KC_F,                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_B,   KC_Z,    KC_X,    KC_C,    KC_V,  _______,   _______, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_I,    KC_M,    KC_GRV,KC_LALT, MO(3),     KC_SPC,  KC_RALT, KC_RCTL,KC_MINS, KC_EQL 
+                    KC_I,    KC_M,   MO(3), KC_LALT, KC_SPC,     MO(2), KC_ENT,  KC_RCTL, KC_MINS, KC_EQL 
 ),
 
 [NAVI] = LAYOUT( //navigation
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______,  KC_DEL,
-  _______, _______, KC_PGUP, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_PGUP, _______, _______, _______,                   CT_LEFT, _______, _______, CT_RGHT, _______, _______,
   _______, KC_HOME, KC_PGDN, KC_END,  _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [FUNC] = LAYOUT( //function
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11,  
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11,  
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_F12,
-  _______, _______, _______, _______, _______, _______,                   CT_LEFT, _______, _______, CT_RGHT, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, _______, TO(1),
                     _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, TO(0),   _______, TG(5)
 ),
