3f437384eb16887c8fd70c737f70609baf9d0fb6
Nick Scratch <drevilbones@gmail.com>
2025-12-18 17:45:15 -0500

    experiments for ps/2 trackball integration

--- keyboards/handwired/onekey/rp2040/mcuconf.h
+++ keyboards/handwired/onekey/rp2040/mcuconf.h
@@ -13,3 +13,5 @@
 
 #undef RP_SPI_USE_SPI0
 #define RP_SPI_USE_SPI0 TRUE
+
+//#define PS2_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the PS2 implementation uses the PIO0 peripheral
